5d52a32182ae09065972344fe0bf35275edf2088
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-03-28 09:49:55 +0100

    Add CORS_ALLOWED_ORIGINS

--- api/settings.py
+++ api/settings.py
@@ -65,7 +65,11 @@ ALLOWED_HOSTS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'https://flashapp-9f1ef2d8a1e3.herokuapp.com',
 
+]
 # Application definition
 
 INSTALLED_APPS = [
