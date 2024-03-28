42f6002b1c282071165671709b81a9180fbaf48a
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-03-28 09:18:10 +0100

    Add logout_route to urls

--- api/urls.py
+++ api/urls.py
@@ -21,9 +21,10 @@ from .views import root_route, logout_route
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('api-auth/', include('rest_framework.urls')),
     path('profiles/', include('profiles.urls')),
     path('posts/', include('posts.urls')),
     path('followers/', include('followers.urls')),
