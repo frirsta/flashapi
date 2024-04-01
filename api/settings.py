d6f11b8afc6883ce0aa937441990b117c53d1a62
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-04-01 17:51:14 +0200

    Update settings

--- api/settings.py
+++ api/settings.py
@@ -41,10 +41,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [(
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-    )],
+    ),
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
         'PAGE_SIZE': 10,
